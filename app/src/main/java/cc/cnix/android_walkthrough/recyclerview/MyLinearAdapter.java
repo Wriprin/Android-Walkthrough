d8eb8a9b587fd5f6d30e108083755993eefb1e19
Wriprin <wriprin@gmail.com>
2022-12-15 15:11:28 +0800

    RecyclerView with Multi ViewHolders

--- app/src/main/java/cc/cnix/android_walkthrough/recyclerview/MyLinearAdapter.java
+++ app/src/main/java/cc/cnix/android_walkthrough/recyclerview/MyLinearAdapter.java
@@ -18,7 +18,7 @@ import cc.cnix.android_walkthrough.R;
  * @Date: 2022/12/14 14:12
  * @Version 1.0
  */
-public class MyLinearAdapter extends RecyclerView.Adapter<MyLinearAdapter.LinearViewHolder> {
+public class MyLinearAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context mContext;
     private OnItemClickListener mListener;
@@ -28,16 +28,39 @@ public class MyLinearAdapter extends RecyclerView.Adapter<MyLinearAdapter.Linear
         this.mListener = listener;
     }
 
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position % 2 != 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
     @NonNull
     @Override
-    public MyLinearAdapter.LinearViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new LinearViewHolder(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item, null));
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == 0) {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item, parent, false);
+            return new LinearViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.layout_linear_alter_item, parent, false);
+            return new LinearViewHolderAlter(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyLinearAdapter.LinearViewHolder holder, @SuppressLint("RecyclerView") int position) {
-        holder.mTvUserName.setText("@Wriprin");
-        holder.mTvUserDesc.setText("Wriprin's BLOG: https://blog.cnix.cc/");
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+
+        if (holder instanceof LinearViewHolder) {
+            LinearViewHolder linearViewHolder = (LinearViewHolder) holder;
+            linearViewHolder.mTvUserName.setText("@Wriprin");
+            linearViewHolder.mTvUserDesc.setText("Wriprin's BLOG: https://blog.cnix.cc/");
+        } else if (holder instanceof LinearViewHolderAlter) {
+            LinearViewHolderAlter linearViewHolderAlter = (LinearViewHolderAlter) holder;
+            linearViewHolderAlter.mTvDesc.setText("这是第二段样式");
+        }
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +75,9 @@ public class MyLinearAdapter extends RecyclerView.Adapter<MyLinearAdapter.Linear
         return 10;
     }
 
+    /**
+     * ViewHolder 0 - 个人介绍样式
+     */
     class LinearViewHolder extends RecyclerView.ViewHolder {
 
         private TextView mTvUserName;
@@ -62,6 +88,19 @@ public class MyLinearAdapter extends RecyclerView.Adapter<MyLinearAdapter.Linear
 
             mTvUserName = itemView.findViewById(R.id.tv_01);
             mTvUserDesc = itemView.findViewById(R.id.tv_02);
+        }
+    }
+
+    /**
+     * ViewHolder 1 - 单段文字样式
+     */
+    class LinearViewHolderAlter extends RecyclerView.ViewHolder {
+
+        private TextView mTvDesc;
+
+        public LinearViewHolderAlter(@NonNull View itemView) {
+            super(itemView);
+            mTvDesc = itemView.findViewById(R.id.tv_01);
         }
     }
 
